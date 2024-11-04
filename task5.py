d9dcc872a55f9a797197714f8721ee16e796e377
aliensbright <144503232+aliensbright@users.noreply.github.com>
2024-11-04 11:32:41 -0800

    added get item, still need to work on drop item

--- task5.py
+++ task5.py
@@ -4,6 +4,8 @@ that inventory.  If they choose 'show inventory' give them a list of the items t
 to make use of the string.split() method in order to separate the item name from the action that precedes it."""
 
 import math
+import random
+
 
 inventory = { # Quantity : Item
     'food'  :4,
@@ -21,31 +23,54 @@ inventory = { # Quantity : Item
     'wool'  :1,
 
     }
+global items
+items = ('food' ,'water','rope' ,'torch','sack' ,'wood' ,'iron' ,'steel','ginger','garlic','fish' ,'stone','wool')
 
 def showInventory():
-    pass
-
-def getItem():
-    pass
+    print("You Have:")
+    for i in inventory:
+        if inventory[i]==0:
+            pass
+        else:
+            print("",inventory[i], i)
 
 def dropItem():
-    pass
+    x=input("Do you want to drop any items?")
+
+def getItem():
+    i = random.randint(0,12)
+    print("You have found a piece of", items[i], "on the ground.\nDo you want to pick it up?")
+    while True:
+        x = input()
+        if x=="yes":
+            inventory[items[i]] += 1
+            break
+        elif x=="no":
+            break
+        elif x=="show inventory":
+            showInventory()
+        else:
+            print("Invalid input, please try again")
+    print("i did it maybe")
+
+
 
 print('Welcome to The Real World \n\n Inputs: \n Use "get item" to add the item to your inventory\n Use "drop item" to drop the item from your inventory\n Use "show inventory" to show how many items you have in your inventory')
-    
+
 
 while True:
+    getItem()
     x = input()
     if x=="get item":
-        pass
+        getItem()
     elif x=="drop item":
-        pass
+        dropItem()
     elif x=="show inventory":
-        print("You Have:")
-        for i in inventory:
-            if inventory[i]==0:
-                pass
-            else:
-                print("",inventory[i], i)
+        showInventory()
     else:
+        for i in items:
+            if x=="drop" + i:
+                inventory[i] -= 1
+                showInventory()
+
         print("Invalid input, please try again")
--- zChallenge.py
+++ zChallenge.py
@@ -1,14 +1,14 @@
 #!python3
 # Explain what this code does
 
-import random
-x = []
+import random #importing the random module allows us to get random values
+x = [] #x is an empty list
 for i in range(40): #return 40 random values
-    if random.randint(0,1): #if value is 1, meaning true, append a random integer between one and ten
-        x.append(random.randint(1,10))
-    else: #if value is 0, meaning false, append a random decimal number that is achieved by adding an integer between 1 and ten
-        #to decimal between 0.1 and 1.0
-        x.append(random.randint(1,10) + random.randint(1,10)/10)
+    if random.randint(0,1): #if value is 1, meaning true, do the code below
+        x.append(random.randint(1,10)) #append a random integer between one and ten to the list x
+    else: #if value is 0, meaning false perform this code below
+        x.append(random.randint(1,10) + random.randint(1,10)/10) #append a random decimal number to list x that is
+        #achieved by adding an integer between 1 and ten to a decimal between 0.1 and 1.0
 
 print(x) #print the completed list with 40 values
 
